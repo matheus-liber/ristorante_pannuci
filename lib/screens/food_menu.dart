1405f5cdde42b61430a0fa86c0033ff3bc6bd14d
matheus-liber <matheuslibergt@gmail.com>
2023-08-28 14:27:18 -0300

    Módulo 2.1 - Desafio criando aba de Menu

---
+++ lib/screens/food_menu.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/components/food_item.dart';
+
+import '../cardapio.dart';
+
+class FoodMenu extends StatelessWidget {
+  const FoodMenu({Key? key}) : super(key: key);
+  final List items = comidas;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: CustomScrollView(slivers: <Widget>[
+        const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Menu',
+                style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
+                textAlign: TextAlign.center,
+              ),
+            )),
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return FoodItem(
+                  itemTitle: items[index]['name'],
+                  itemPrice: items[index]['price'],
+                  imageURI: items[index]['image']);
+              }, childCount: items.length))
+      ]),
+    );
+  }
+}
