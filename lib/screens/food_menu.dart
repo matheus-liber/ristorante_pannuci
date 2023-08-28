3b2902b441fe793783b3a4f3c77ee3dc943f162a
matheus-liber <matheuslibergt@gmail.com>
2023-08-28 15:14:25 -0300

    Módulo 3 - Construindo o menu de bebidas usando grid

--- lib/screens/food_menu.dart
+++ lib/screens/food_menu.dart
@@ -13,21 +13,22 @@ class FoodMenu extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
       child: CustomScrollView(slivers: <Widget>[
         const SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 16.0),
-              child: Text(
-                'Menu',
-                style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
-                textAlign: TextAlign.center,
-              ),
-            )),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              'Menu',
+              style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
         SliverList(
             delegate: SliverChildBuilderDelegate((context, index) {
-              return FoodItem(
-                  itemTitle: items[index]['name'],
-                  itemPrice: items[index]['price'],
-                  imageURI: items[index]['image']);
-              }, childCount: items.length))
+          return FoodItem(
+              itemTitle: items[index]['name'],
+              itemPrice: items[index]['price'],
+              imageURI: items[index]['image']);
+        }, childCount: items.length))
       ]),
     );
   }
